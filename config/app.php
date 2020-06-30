3eb2f4e2acf090cfd01fe6582a1c7d15cb0da678
HuDaming <40834106@qq.com>
2020-06-30 10:47:39 +0800

    后台初始化

--- config/app.php
+++ config/app.php
@@ -228,7 +228,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Debugcar' => Barryvdh\Debugbar\Facade::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
